fdd4b4c657ce5237814b54ea21c1b9397ddc9c4c
dominOman <dominoma013@gmail.com>
2022-05-11 01:28:03 +0200

    test commit

--- src/main/java/com/github/pawelbogdan/aim_battle/AimBattleApplication.java
+++ src/main/java/com/github/pawelbogdan/aim_battle/AimBattleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AimBattleApplication {
-
+    //mainclass
     public static void main(String[] args) {
         SpringApplication.run(AimBattleApplication.class, args);
     }
