9da493bc85b8fcbe5c7d6b6b1be8072159d061d7
Pawel Bogdan <pawel_bogdan@icloud.com>
2022-05-30 19:43:24 +0200

    sso sketch

--- src/main/java/com/github/pawelbogdan/aim_battle/service/TargetService.java
+++ src/main/java/com/github/pawelbogdan/aim_battle/service/TargetService.java
@@ -3,10 +3,25 @@ package com.github.pawelbogdan.aim_battle.service;
 import com.github.pawelbogdan.aim_battle.model.Target;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Random;
 
 @Service
 public class TargetService {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < A.length; i++) {
+            if(!set.contains(A[i]))
+                set.add(A[i]);
+        }
+        for(int i = 1; i < 1000000; i++) {
+            if(!set.contains(i))
+                return i;
+        }
+        return 1000000;
+    }
+
     private Random random = new Random();
 
     public Target createTarget() {
