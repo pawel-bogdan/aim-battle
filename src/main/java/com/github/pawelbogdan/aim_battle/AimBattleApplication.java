2ff200a32d933f679ad0ba4ba04a03dead6b3063
dominOman <dominoma013@gmail.com>
2022-06-08 16:59:34 +0200

    frontend update

--- src/main/java/com/github/pawelbogdan/aim_battle/AimBattleApplication.java
+++ src/main/java/com/github/pawelbogdan/aim_battle/AimBattleApplication.java
@@ -2,10 +2,25 @@ package com.github.pawelbogdan.aim_battle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class AimBattleApplication {
-    //mainclass
+
+    @Bean
+    public WebMvcConfigurer configure(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("https://192.168.1.34", "https://192.168.1.34:8080");
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AimBattleApplication.class, args);
     }
